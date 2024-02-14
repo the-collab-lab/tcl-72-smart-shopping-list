2d6ebb5fad5d355835ef3a330a005d6f2edb05ca
Jamila Mahamed <j.mahamed95@icloud.com>
2024-02-14 20:08:11 +0100

    created form

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,27 @@
 import { SingleList } from '../components/SingleList';
 import './Home.css';
+import { createList } from '../api/firebase';
+import { useState } from 'react';
 
 export function Home({ data, setListPath }) {
+	// const [createList,setCreateList] = useState(false)
+
+	// function onSubmit() {
+	// 	useState = true;
+	// }
+
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
+
+			<form id="list-form" action="#">
+				<label htmlFor="create-list">Create a new list</label>
+				<input type="text" id="create-list" />
+				<input type="submit" />
+			</form>
+
 			<ul>
 				{/**
 				 * TODO: write some JavaScript that renders the `lists` array
