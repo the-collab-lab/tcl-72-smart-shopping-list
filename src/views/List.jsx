5104b46a9783d7428171161473d56c5ca7532516
khadijahnabil <nabilkhadijah7@gmail.com>
2024-02-25 11:35:42 +0100

    removed ESLint warnings

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -5,23 +5,21 @@ export function List({ data }) {
 	const [searchInput, setSearchInput] = useState('');
 	const [filteredItems, setFilteredItems] = useState([]);
 
-	// Function to filter items based on search input
-	const filterItems = (searchInput) => {
-		const searchResult = data.filter((item) =>
-			item.name.toLowerCase().includes(searchInput.toLowerCase()),
-		);
-		setFilteredItems(searchResult);
-	};
-
 	useEffect(() => {
 		// Initialize filteredItems with the entire data array when the component mounts
 		setFilteredItems(data);
 	}, [data]);
 
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+	// Function to filter items based on search input
 	useEffect(() => {
+		const filterItems = (searchInput) => {
+			const searchResult = data.filter((item) =>
+				item.name.toLowerCase().includes(searchInput.toLowerCase()),
+			);
+			return setFilteredItems(searchResult);
+		};
 		filterItems(searchInput);
-	}, [searchInput]);
+	}, [searchInput, data]);
 
 	// Function to clear the search input
 	const clearSearchInput = () => {
