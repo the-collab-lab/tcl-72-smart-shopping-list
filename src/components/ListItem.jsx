05a057f6aad64fe484c26aceb0f0846392bfcace
Jamila Mahamed <j.mahamed95@icloud.com>
2024-03-02 22:07:42 +0100

    checkbox unchecks after 24 hrs

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,13 +1,48 @@
 import './ListItem.css';
 import { updateItem } from '../api/firebase';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export function ListItem({ id, name, listPath }) {
 	const [itemChecked, setItemChecked] = useState(false);
 
+	useEffect(() => {
+		const lastCheckedTime = localStorage.getItem(`checked_${id}`);
+		if (lastCheckedTime) {
+			// const oneMinute = 60 * 1000; 1 minute
+			const twentyFourHours = 24 * 60 * 60 * 1000;
+			const currentTime = new Date().getTime();
+			if (currentTime - parseInt(lastCheckedTime) < twentyFourHours) {
+				setItemChecked(true);
+			}
+		}
+	}, [id]);
+
+	// if (currentTime - parseInt(lastCheckedTime) < oneMinute) {
+	// 	setItemChecked(true);
+	// }
+
+	useEffect(() => {
+		if (itemChecked) {
+			const timer = setTimeout(
+				() => {
+					setItemChecked(false);
+					localStorage.removeItem(`checked_${id}`);
+				},
+				24 * 60 * 60 * 1000,
+			);
+			// 60 * 1000 1 minute test
+			return () => clearTimeout(timer);
+		}
+	}, [itemChecked, id]);
+
 	const setItemPurchased = () => {
 		setItemChecked(!itemChecked);
 		updateItem(listPath, id);
+		if (!itemChecked) {
+			localStorage.setItem(`checked_${id}`, new Date().getTime().toString());
+		} else {
+			localStorage.removeItem(`checked_${id}`);
+		}
 	};
 
 	return (
@@ -16,8 +51,8 @@ export function ListItem({ id, name, listPath }) {
 				<input
 					name={name}
 					type="checkbox"
-					value={itemChecked}
-					onChange={() => setItemPurchased()}
+					checked={itemChecked}
+					onChange={setItemPurchased}
 				/>
 				{name}
 			</label>
